8ea821eb8ab99794997f749be15d9b9ac1ca41c6
alesandramempis <alesandramempis@gmail.com>
2023-02-26 20:51:22 -0300

    Criação de card

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,13 +3,26 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import { Aleatorio } from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
+import "./App.css";
 
 export default function App(props) {
   return (
     <>
-      <ComParametro titulo="Situação do Aluno" aluno="Yuna" nota={9.3} />
-      <Primeiro></Primeiro>
-      <Aleatorio minimo={5} maximo={18} />
+      <div className="App">
+        <h1>Fundamentos React</h1>
+        <div className="Cards">
+          <Card titulo="#1 Primeiro componente">
+            <Primeiro></Primeiro>
+          </Card>
+          <Card titulo="#2 Com Parâmetro">
+            <ComParametro titulo="Situação do Aluno" aluno="Yuna" nota={9.3} />
+          </Card>
+          <Card titulo="#3 Exemplo de card">
+            <Aleatorio minimo={5} maximo={18} />
+          </Card>
+        </div>
+      </div>
     </>
   );
 }

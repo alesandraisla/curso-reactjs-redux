2e575728e6ba30bb5e2433b3c3373fae56ebe36c
alesandramempis <alesandramempis@gmail.com>
2023-02-26 21:04:22 -0300

    Estilos dinâmicos

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -12,13 +12,13 @@ export default function App(props) {
       <div className="App">
         <h1>Fundamentos React</h1>
         <div className="Cards">
-          <Card titulo="#1 Primeiro componente">
+          <Card titulo="#1 Primeiro componente" color="#011f4b  ">
             <Primeiro></Primeiro>
           </Card>
-          <Card titulo="#2 Com Parâmetro">
+          <Card titulo="#2 Com Parâmetro" color="#03396c  ">
             <ComParametro titulo="Situação do Aluno" aluno="Yuna" nota={9.3} />
           </Card>
-          <Card titulo="#3 Exemplo de card">
+          <Card titulo="#3 Exemplo de card" color="#005b96 ">
             <Aleatorio minimo={5} maximo={18} />
           </Card>
         </div>
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import "./Card.css";
 
 export default function Card(props) {
+  const style = {
+    backgroundColor: props.color || "navy",
+    borderColor: props.color || "navy",
+  };
   return (
-    <div className="Card">
+    <div className="Card" style={style}>
       <div className="Title">{props.titulo}</div>
       <div className="Content">{props.children}</div>
     </div>

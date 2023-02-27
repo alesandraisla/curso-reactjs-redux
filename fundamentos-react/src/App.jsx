62e72c25b24b9292c8144f2902ba0e6f715f0a6b
alesandramempis <alesandramempis@gmail.com>
2023-02-26 21:57:12 -0300

    Propriedades entre componentes

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,6 +5,8 @@ import ComParametro from "./components/basicos/ComParametro";
 import { Aleatorio } from "./components/basicos/Aleatorio";
 import Card from "./components/layout/Card";
 import "./App.css";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 export default function App(props) {
   return (
@@ -15,11 +17,18 @@ export default function App(props) {
           <Card titulo="#1 Primeiro componente" color="#011f4b  ">
             <Primeiro></Primeiro>
           </Card>
-          <Card titulo="#2 Com Parâmetro" color="#03396c  ">
+          <Card titulo="#2 Com Parâmetro" color="#ff8b94  ">
             <ComParametro titulo="Situação do Aluno" aluno="Yuna" nota={9.3} />
           </Card>
-          <Card titulo="#3 Exemplo de card" color="#005b96 ">
+          <Card titulo="#3 Exemplo de card" color="#008744  ">
             <Aleatorio minimo={5} maximo={18} />
+          </Card>
+          <Card titulo="#4 Componente com filhos - Familia" color="#f37735">
+            <Familia sobrenome="Ferreira">
+              <FamiliaMembro nome="Julia" />
+              <FamiliaMembro nome="Alice" />
+              <FamiliaMembro nome="Bryan" />
+            </Familia>
           </Card>
         </div>
       </div>

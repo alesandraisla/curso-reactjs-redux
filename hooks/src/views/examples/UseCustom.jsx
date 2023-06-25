99f4b81a8f8f5d1fea0d476b843388db1af36f64
alesandramempis <alesandramempis@gmail.com>
2023-06-25 16:47:03 -0300

    Criando um hook configurável

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,15 +1,50 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import { useCounter } from "../../hooks/useCounter";
+import { useFetch } from "../../hooks/useFetch";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseCustom">
-            <PageTitle
-                title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
-            />
-        </div>
-    )
-}
+  const [count, inc, dec] = useCounter();
+  const url = "http://files.cod3r.com.br/curso-react/estados.json";
+  const response = useFetch(url);
 
-export default UseRef
+  function showStates(states) {
+    return states.map((state) => (
+      <li key={state.nome}>
+        {state.nome} - {state.sigla}
+      </li>
+    ));
+  }
+
+  return (
+    <div className="UseCustom">
+      <PageTitle
+        title="Seu Hook"
+        subtitle="Vamos aprender como criar o nosso próprio Hook!"
+      />
+      <div>
+        <div className="center">
+          <span className="text">{count}</span>
+          <div>
+            <button className="btn" onClick={() => inc()}>
+              + 1
+            </button>
+          </div>
+          <div>
+            <button className="btn" onClick={() => dec()}>
+              - 1
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="center">
+          <ul>{!response.loading ? showStates(response.data) : false}</ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;

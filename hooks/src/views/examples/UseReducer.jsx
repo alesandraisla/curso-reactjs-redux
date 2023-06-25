5d33e5716b87ed6630c296e3c47b8b298b898a59
alesandramempis <alesandramempis@gmail.com>
2023-06-25 11:39:44 -0300

    Exercicio com useReducer

--- hooks/src/views/examples/UseReducer.jsx
+++ hooks/src/views/examples/UseReducer.jsx
@@ -17,10 +17,34 @@ function reducer(stateActual, action) {
       };
     case "login":
       return { ...stateActual, user: { name: action.payload } };
+    case "numberMultiplicar7":
+      return {
+        ...stateActual,
+        number: stateActual.number * 7,
+      };
+    case "dividir25":
+      return {
+        ...stateActual,
+        number: stateActual.number / 25,
+      };
+    case "numberParseInt":
+      return {
+        ...stateActual,
+        number: parseInt(stateActual.number),
+      };
+    case "numberAddN":
+      return {
+        ...stateActual,
+        number: stateActual.number + action.payload,
+      };
     default:
       return stateActual;
   }
 }
+// Criar uma ação pra multiplicar por 7
+// dividir numero atual por 25
+// ParseInt do numero e transformar pra inteiro
+// Adicionar numero n (qualquer) somado flexivel numero adicionar n login valor
 
 const UseReducer = (props) => {
   //No parametro precisa passar no parametro sempre a function que vai evluir e o estado inicial
@@ -50,6 +74,30 @@ const UseReducer = (props) => {
             onClick={() => dispatch({ type: "numberAdd2" })}
           >
             +2
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberMultiplicar7" })}
+          >
+            Multiplicar por 7
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "dividir25" })}
+          >
+            Dividir por 25
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberParseInt" })}
+          >
+            Arredondar
+          </button>
+          <button
+            className="btn"
+            onClick={() => dispatch({ type: "numberAddN", payload: -9 })}
+          >
+            -9
           </button>
         </div>
       </div>
